466804a938681b0a49af620277556d1361ac5b7c
Leo Spratt <enchant97@users.noreply.github.com>
2021-07-22 22:11:11 +0100

    add downloading archive of HEAD

--- git_web/main.py
+++ git_web/main.py
@@ -2,7 +2,9 @@ import os
 from pathlib import Path
 
 from quart import Quart, abort, render_template
+from quart import make_response
 
+from .git.archive import ArchiveTypes, run_get_archive
 from .git.log import run_get_logs
 
 app = Quart(__name__)
@@ -20,3 +22,20 @@ async def repo_logs(repo_name: str):
         logs=logs,
         repo_name=repo_name
     )
+
+
+@app.route("/repo/<repo_name>/archive.<archive_type>")
+async def repo_archive(repo_name: str, archive_type: str):
+    try:
+        ArchiveTypes(archive_type)
+    except ValueError:
+        abort(404)
+
+    repo_path = REPOS_PATH / (repo_name + ".git")
+    if not repo_path.exists():
+        abort(404)
+
+    content = run_get_archive(repo_path, archive_type)
+    response = await make_response(content)
+    response.mimetype = "application/" + archive_type
+    return response
