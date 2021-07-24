f850a15c1996d4745615870b24275075d28f0b91
Leo Spratt <enchant97@users.noreply.github.com>
2021-07-24 14:41:59 +0100

    add basic login auth

--- git_web/main.py
+++ git_web/main.py
@@ -1,21 +1,50 @@
 import os
+import secrets
+import shutil
 from pathlib import Path
 
 from quart import (Quart, abort, make_response, redirect, render_template,
                    request, url_for)
+from quart_auth import AuthManager, AuthUser, Unauthorized, login_required, login_user
 
 from .git.archive import ArchiveTypes, run_get_archive
 from .git.log import run_get_logs
-from .git.utils import find_repos, init_repo, get_description, set_description
-import shutil
+from .git.utils import find_repos, get_description, init_repo, set_description
+
 app = Quart(__name__)
+auth_manager = AuthManager(app)
 
 REPOS_PATH = Path(os.environ["REPOS_PATH"])
 # should look similar to: git@example.com
 REPOS_SSH_BASE = os.environ["REPOS_SSH_BASE"]
+# password preventing unwanted access
+LOGIN_PASSWORD = os.environ["LOGIN_PASSWORD"]
+app.secret_key = os.environ["SECRET_KEY"]
+# this is allowing us to run through a proxy
+app.config["QUART_AUTH_COOKIE_SECURE"] = False
+
+
+@app.errorhandler(Unauthorized)
+async def redirect_to_login(*_):
+    return redirect(url_for(".do_login"))
+
+
+@app.route("/login", methods=["GET", "POST"])
+async def do_login():
+    if request.method == "POST":
+        password = (await request.form).get("password")
+        if not password:
+            abort(400)
+        if not secrets.compare_digest(password, LOGIN_PASSWORD):
+            abort(401)
+        login_user(AuthUser("user"), True)
+        return redirect(url_for(".directory_list"))
+
+    return await render_template("login.html")
 
 
 @app.route("/")
+@login_required
 async def directory_list():
     return await render_template(
         "directories.html",
@@ -23,6 +52,7 @@ async def directory_list():
     )
 
 @app.route("/new", methods=["POST"])
+@login_required
 async def new_directory():
     repo_dir = (await request.form).get("repo-dir")
 
@@ -40,6 +70,7 @@ async def new_directory():
 
 
 @app.route("/<repo_dir>/new", methods=["POST"])
+@login_required
 async def repo_init(repo_dir: str):
     if not (REPOS_PATH / repo_dir).exists():
         abort(404)
@@ -59,6 +90,7 @@ async def repo_init(repo_dir: str):
 
 
 @app.route("/<directory>/repos")
+@login_required
 async def repo_list(directory):
     repo_paths = find_repos(REPOS_PATH / directory, True)
     return await render_template(
@@ -69,6 +101,7 @@ async def repo_list(directory):
 
 
 @app.route("/<repo_dir>/repos/<repo_name>")
+@login_required
 async def repo_view(repo_dir: str, repo_name: str):
     repo_path = REPOS_PATH / repo_dir / (repo_name + ".git")
     if not repo_path.exists():
@@ -85,6 +118,7 @@ async def repo_view(repo_dir: str, repo_name: str):
 
 
 @app.route("/<repo_dir>/repos/<repo_name>/delete", methods=["GET"])
+@login_required
 async def repo_delete(repo_dir: str, repo_name: str):
     repo_path = REPOS_PATH / repo_dir / (repo_name + ".git")
     if not repo_path.exists():
@@ -94,6 +128,7 @@ async def repo_delete(repo_dir: str, repo_name: str):
 
 
 @app.route("/<repo_dir>/repos/<repo_name>/set-description", methods=["POST"])
+@login_required
 async def repo_set_description(repo_dir: str, repo_name: str):
     repo_path = REPOS_PATH / repo_dir / (repo_name + ".git")
     if not repo_path.exists():
@@ -109,6 +144,7 @@ async def repo_set_description(repo_dir: str, repo_name: str):
 
 
 @app.route("/<repo_dir>/repos/<repo_name>/commits")
+@login_required
 async def repo_commit_log(repo_dir: str, repo_name: str):
     repo_path = REPOS_PATH / repo_dir / (repo_name + ".git")
     if not repo_path.exists():
@@ -123,6 +159,7 @@ async def repo_commit_log(repo_dir: str, repo_name: str):
 
 
 @app.route("/<repo_dir>/repos/<repo_name>/archive.<archive_type>")
+@login_required
 async def repo_archive(repo_dir: str, repo_name: str, archive_type: str):
     try:
         ArchiveTypes(archive_type)
